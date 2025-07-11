beec5deb9ccac1ea02ed50c103f7d05eca5e7ff3
Davide Bassi <davide.bassi314@gmail.com>
2025-07-11 17:57:53 +0200

    feat: implement both js and gzipped bots
    The underlying bot type can also not be application/javascript, but I don't think it's possible to dynamically import other types?
    Well, this works much better then the previous approach of just gzip, since it also allows less modern browsers to use the app!

--- frontend/src/inflate.ts
+++ frontend/src/inflate.ts
@@ -23,18 +23,25 @@ export async function inflate(blob: Blob) {
     /* @ts-ignore */
     const __vite__injectQuery = url => url;
 
-    // See: https://stackoverflow.com/a/68829631
-    const ungzip = new DecompressionStream('gzip');
-    const stream = blob.stream().pipeThrough(ungzip);
-    
+    // By default let jelly be the blob
+    let jelly: Blob = blob;
+
+    // If it's a gzip file, decompress it
+    if (blob.type === 'application/gzip') {
+      
+      // See: https://stackoverflow.com/a/68829631
+      const ungzip = new DecompressionStream('gzip');
+      const stream = blob.stream().pipeThrough(ungzip);
+      
+      // See: https://stackoverflow.com/a/50875615
+      jelly = await new Response(stream).blob()
+        .then(blob => blob.slice(0, blob.size, "application/javascript"));
+    }
+
+    // Encode the blob as a data url
+    const url = await encode(jelly);
+
     // Import the stream as a module
-    // See: https://stackoverflow.com/a/50875615
-    return await new Response(stream).blob()
-        .then(blob => blob.slice(0, blob.size, "application/javascript"))
-        // We can't use blob URLs within cerved, WTF!?
-        //.then(blob => URL.createObjectURL(blob))
-        // Fuck them, we can just encode to data url!
-        .then(blob => encode(blob))
-        .then(url => import(/* @vite-ignore */ url));
+    return await import(/* @vite-ignore */ url);
 
 }
